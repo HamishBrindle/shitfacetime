741eebd27433d1e9013e3af140c0124af23c68b3
HamishBrindle <hamishbrindle@gmail.com>
2017-12-01 15:04:59 -0800

    Adding Justin's changes

---
+++ README.md
@@ -0,0 +1,2 @@
+# SFT
+A streaming website to connect with groups of people who want to pre-drink all over the world.
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+    res.render('hello');
+});
