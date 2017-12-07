bd30de6ba08dbf1135e9d39c7c8d9d98eb35324a
Justin-Leung <lngventure@gmail.com>
2017-12-07 12:15:00 -0800

    Updated hamishs changes.

--- public/javascripts/shitfacetime.js
+++ public/javascripts/shitfacetime.js
@@ -17,28 +17,33 @@ var peerSettings = {　
     port: 443,
     debug: 3,
     config: {
-        'iceServers': [
-            {
+        'iceServers': [{
                 url: "stun:global.stun.twilio.com:3478?transport=udp"
             },
             {
                 url: "turn:global.turn.twilio.com:3478?transport=udp",
-                username: "49e1aa5f04c9ef9e20fd3d152dc45fc9952407453a2b5f5b6d2a2c34fff2a6ee",
-                credential: "vhGp1LbBos0Qliz464N1Ui1bqxI0r/7ID/0F3K0ry3g="
+                username: "6126da033a7be666f10a3f1c0a4bf26f0111c0ac7e7ebd9e3503a181f9e51d8c",
+                credential: "bM0+RW4MfPkSJRkGMtdo26vI36/USjTt6zG4Eaitt8s="
             },
             {
                 url: "turn:global.turn.twilio.com:3478?transport=tcp",
-                username: "49e1aa5f04c9ef9e20fd3d152dc45fc9952407453a2b5f5b6d2a2c34fff2a6ee",
-                credential: "vhGp1LbBos0Qliz464N1Ui1bqxI0r/7ID/0F3K0ry3g="
+                username: "6126da033a7be666f10a3f1c0a4bf26f0111c0ac7e7ebd9e3503a181f9e51d8c",
+                credential: "bM0+RW4MfPkSJRkGMtdo26vI36/USjTt6zG4Eaitt8s="
             },
             {
                 url: "turn:global.turn.twilio.com:443?transport=tcp",
-                username: "49e1aa5f04c9ef9e20fd3d152dc45fc9952407453a2b5f5b6d2a2c34fff2a6ee",
-                credential: "vhGp1LbBos0Qliz464N1Ui1bqxI0r/7ID/0F3K0ry3g="
+                username: "6126da033a7be666f10a3f1c0a4bf26f0111c0ac7e7ebd9e3503a181f9e51d8c",
+                credential: "bM0+RW4MfPkSJRkGMtdo26vI36/USjTt6zG4Eaitt8s="
             }
         ]
     }
 };
+
+// Is our confetti showing?
+var isConfetti = false;
+
+// Is our confetti showing?
+var isMusic = false;
 
 // Setup our Howler.js Object
 // for playing sounds
@@ -50,6 +55,31 @@ var sound = new Howl({
     volume: 0.5,
     onend: function() {
         console.log('Finished playing sound.');
+    }
+});
+
+// for playing kids celebrating with confetti
+var kids = new Howl({
+    src: ['sound/yayy.mp3'],
+    sprite: {
+        yayy: [0, 5000]
+    },
+    volume: 0.25,
+    onend: function() {
+        console.log('Finished playing sound.');
+    }
+});
+
+// for playing music
+var music = new Howl({
+    src: ['sound/gucci-gang.mp3'],
+    sprite: {
+        gucci: [16000, 4075]
+    },
+    volume: 0.5,
+    onend: function() {
+        console.log('Finished playing music.');
+        isMusic = false;
     }
 });
 
@@ -355,12 +385,28 @@ socket.on('tool selection', function(data) {
         sound.play('blast');
     } else if (data.tool === 'tool-music') {
         console.log('Tool recieved: music.');
+        if (!isMusic) {
+            isMusic = true;
+            music.play('gucci');
+        }
     } else if (data.tool === 'tool-thumbsup') {
         console.log('Tool recieved: thumbs-up.');
     } else if (data.tool === 'tool-thumbsdown') {
         console.log('Tool recieved: thumbs-down.');
-    } else if (data.tool === 'tool-heart') {
-        console.log('Tool recieved: heart.');
+    } else if (data.tool === 'tool-confetti') {
+        console.log('Tool recieved: confetti.');
+        // Confetti is always playing. Here we decide whether or note
+        // to show it. The timeout is for hiding it once it's done.
+        // TODO: Clean this up
+        if (!isConfetti) {
+            isConfetti = true;
+            $('#confeti').show();
+            kids.play('yayy');
+            setTimeout(function() {
+                $('#confeti').hide();
+                isConfetti = false;
+            }, 5000);
+        }
     }
 
 });
@@ -567,6 +613,8 @@ function onCallLoading() {
  */
 function onCallConnected() {
     $('.video-loader').hide();
+    confetti();
+    $('#confeti').hide();
 }
 
 /**
@@ -701,4 +749,120 @@ function sendChatMessage() {
 
     // Resets the value.
     $('#m').val('');
+}
+
+function confetti() {
+    var COLORS, Confetti, NUM_CONFETTI, PI_2, canvas, confetti, context, drawCircle, drawCircle2, drawCircle3, i, range, xpos;
+    NUM_CONFETTI = 40;
+    COLORS = [
+        [235, 90, 70],
+        [97, 189, 79],
+        [242, 214, 0],
+        [0, 121, 191],
+        [195, 119, 224]
+    ];
+    PI_2 = 2 * Math.PI;
+    canvas = document.getElementById("confeti");
+    context = canvas.getContext("2d");
+    window.w = 0;
+    window.h = 0;
+    window.resizeWindow = function() {
+        window.w = canvas.width = window.innerWidth;
+        return window.h = canvas.height = window.innerHeight
+    };
+    window.resizeWindow();
+    window.addEventListener("resize", resizeWindow, !1);
+    window.onload = function() {
+        return setTimeout(resizeWindow, 0)
+    };
+    range = function(a, b) {
+        return (b - a) * Math.random() + a
+    };
+    drawCircle = function(a, b, c, d) {
+        context.beginPath();
+        context.moveTo(a, b);
+        context.bezierCurveTo(a - 17, b + 14, a + 13, b + 5, a - 5, b + 22);
+        context.lineWidth = 2;
+        context.strokeStyle = d;
+        return context.stroke()
+    };
+    drawCircle2 = function(a, b, c, d) {
+        context.beginPath();
+        context.moveTo(a, b);
+        context.lineTo(a + 6, b + 9);
+        context.lineTo(a + 12, b);
+        context.lineTo(a + 6, b - 9);
+        context.closePath();
+        context.fillStyle = d;
+        return context.fill()
+    };
+    drawCircle3 = function(a, b, c, d) {
+        context.beginPath();
+        context.moveTo(a, b);
+        context.lineTo(a + 5, b + 5);
+        context.lineTo(a + 10, b);
+        context.lineTo(a + 5, b - 5);
+        context.closePath();
+        context.fillStyle = d;
+        return context.fill()
+    };
+    xpos = 0.9;
+    document.onmousemove = function(a) {
+        return xpos = a.pageX / w
+    };
+    window.requestAnimationFrame = function() {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(a) {
+            return window.setTimeout(a, 5)
+        }
+    }();
+    Confetti = function() {
+        function a() {
+            this.style = COLORS[~~range(0, 5)];
+            this.rgb = "rgba(" + this.style[0] + "," + this.style[1] + "," + this.style[2];
+            this.r = ~~range(2, 6);
+            this.r2 = 2 * this.r;
+            this.replace()
+        }
+        a.prototype.replace = function() {
+            this.opacity = 0;
+            this.dop = 0.03 * range(1, 4);
+            this.x = range(-this.r2, w - this.r2);
+            this.y = range(-20, h - this.r2);
+            this.xmax = w - this.r;
+            this.ymax = h - this.r;
+            this.vx = range(0, 2) + 8 * xpos - 5;
+            return this.vy = 0.7 * this.r + range(-1, 1)
+        };
+        a.prototype.draw = function() {
+            var a;
+            this.x += this.vx;
+            this.y += this.vy;
+            this.opacity +=
+                this.dop;
+            1 < this.opacity && (this.opacity = 1, this.dop *= -1);
+            (0 > this.opacity || this.y > this.ymax) && this.replace();
+            if (!(0 < (a = this.x) && a < this.xmax)) this.x = (this.x + this.xmax) % this.xmax;
+            drawCircle(~~this.x, ~~this.y, this.r, this.rgb + "," + this.opacity + ")");
+            drawCircle3(0.5 * ~~this.x, ~~this.y, this.r, this.rgb + "," + this.opacity + ")");
+            return drawCircle2(1.5 * ~~this.x, 1.5 * ~~this.y, this.r, this.rgb + "," + this.opacity + ")")
+        };
+        return a
+    }();
+    confetti = function() {
+        var a, b, c;
+        c = [];
+        i = a = 1;
+        for (b = NUM_CONFETTI; 1 <= b ? a <= b : a >= b; i = 1 <= b ? ++a : --a) c.push(new Confetti);
+        return c
+    }();
+    window.step = function() {
+        var a, b, c, d;
+        requestAnimationFrame(step);
+        context.clearRect(0, 0, w, h);
+        d = [];
+        b = 0;
+        for (c = confetti.length; b < c; b++) a = confetti[b], d.push(a.draw());
+        return d
+    };
+    step();;
 }
